c8784a51d0052963a4d74838b9bf8dedaea5d044
C-Grube <17773100+C-Grube@users.noreply.github.com>
2022-12-12 11:41:48 -0500

    Update linux_mde_install.sh

--- linux_mde_install.sh
+++ linux_mde_install.sh
@@ -2,7 +2,7 @@
 
 # Description: Simple Bash script to install MDE on Debian Linux
 # Instructions: bash linux_install_mde.sh [/path/to/mde-onboarding-file.py]
-#               curl -k https://raw.githubusercontent.com/C-Grube/linux_mde_install/main/linux_mde_install.sh | bash -s -- MicrosoftDefenderATPOnboardingLinuxServer
+#               curl -k https://raw.githubusercontent.com/C-Grube/linux_mde_install/main/linux_mde_install.sh | bash -s -- MicrosoftDefenderATPOnboardingLinuxServer.py
 
 # Install pre-req software
 sudo apt-get install curl
